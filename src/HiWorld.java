1d0dfead522c60e7562b37b648c7b5155d0f2942
jaery11 <727729285@qq.com>
2020-07-08 16:46:19 +0800

    text

---
+++ src/HiWorld.java
@@ -0,0 +1,11 @@
+/**
+ * HiWorld
+ *
+ * @author 72772
+ * @date 2020/7/8 16:44
+ */
+public class HiWorld {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
